b068951ef8bd3d13122f3d35b05d2844736b3d0f
sylveonpoke <playingdigimonlinkz881@ezweb.ne.jp>
2021-12-22 13:06:47 +0900

    事前準備やり忘れ

--- Gemfile
+++ Gemfile
@@ -57,3 +57,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+group :development do
+  gem 'rubocop', require: false
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    family_name           {Faker::Name.initials(number: 2)}
-    first_name            {Faker::Name.initials(number: 2)}
-    family_name_kana      {Faker::Name.initials(number: 2)}
-    first_name_kana       {Faker::Name.initials(number: 2)}
-    date_of_birth         {Faker::Date.birthday(min_age: 18, max_age: 65)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 2) }
+    family_name           { Faker::Name.initials(number: 2) }
+    first_name            { Faker::Name.initials(number: 2) }
+    family_name_kana      { Faker::Name.initials(number: 2) }
+    first_name_kana       { Faker::Name.initials(number: 2) }
+    date_of_birth         { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
   end
 end
